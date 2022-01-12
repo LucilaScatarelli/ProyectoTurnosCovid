4b48d4b548d9715b6ea96d00dd65a51852979793
LucilaScatarelli <lucilascatarelli@gmail.com>
2022-01-12 17:23:36 -0300

    Primera version del formulario

---
+++ turnosCovid.py
@@ -0,0 +1,13 @@
+edad=int(input("Ingrese la edad en números, por favor: "))
+while edad <= 0 or edad>100:
+    print("Edad incorrecta. ")
+    edad=int(input("Ingrese la edad en números, por favor: "))
+
+grupoRiesgo= input("Posee una patologia de base que lo convierta en una persona de grupo de riesgo? (Si/No): ")
+if grupoRiesgo=="si" or grupoRiesgo=="Si" or grupoRiesgo=="SI":
+    patologia=input("Ingrese cual es la patologìa por favor: ")
+ 
+numeroTelefonico=int(input("Ingrese su numero de contacto, por favor: "))
+
+print("Muchas Gracias por completar el formulario de vacunación contra COVID19. Lo contactaremos a la brevedad.")
+  
